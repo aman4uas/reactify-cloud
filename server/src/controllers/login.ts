68111a459315c38fcdb2457679430c1ad88189e2
aman4uas <aman4u.as@gmail.com>
2024-06-18 19:40:26 +0530

    update3

--- server/src/controllers/login.ts
+++ server/src/controllers/login.ts
@@ -1,24 +1,21 @@
-import { Request, Response } from 'express'
-import axios from 'axios'
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
-dotenv.config()
-
-import { User } from '../models'
-import { JWT_EXPIRY_TIME } from '../constants'
+import { Request, Response } from 'express';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import { User } from '../models';
+import { JWT_EXPIRY_TIME } from '../constants';
 
 interface IPayload {
-  username: String
+  username: string;
 }
 
-const githubAuth = async (req: Request, res: Response) => {
-  res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`
-  )
-}
+const githubAuth = (req: Request, res: Response) => {
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+  res.json({ success: true, url: githubAuthUrl });
+};
 
 const callback = async (req: Request, res: Response) => {
   try {
+    console.log("request got !!")
     const result = await axios.post(
       'https://github.com/login/oauth/access_token',
       {
@@ -31,53 +28,53 @@ const callback = async (req: Request, res: Response) => {
           Accept: 'application/json'
         }
       }
-    )
+    );
 
-    const githubAcessToken = result.data.access_token
-    const apiUrl = 'https://api.github.com/user'
+    const githubAccessToken = result.data.access_token;
+    const apiUrl = 'https://api.github.com/user';
 
     const response = await axios.get(apiUrl, {
       headers: {
-        Authorization: `Bearer ${githubAcessToken}`
+        Authorization: `Bearer ${githubAccessToken}`
       }
-    })
+    });
 
-    const currentUser = await User.findOne({ username: response.data.login })
+    const currentUser = await User.findOne({ username: response.data.login });
 
-    const payload: IPayload = { username: response.data.login }
+    const payload: IPayload = { username: response.data.login };
     const userAccessToken = jwt.sign(payload, process.env.JWT_SECRET!, {
       expiresIn: JWT_EXPIRY_TIME
-    })
+    });
 
     if (currentUser) {
-      const updatedUser = await User.findOneAndUpdate(
+      await User.findOneAndUpdate(
         { username: response.data.login },
-        { gitHubToken: githubAcessToken },
+        { gitHubToken: githubAccessToken },
         { new: true }
-      )
+      );
     } else {
       const newUser = new User({
         name: response.data.name || 'GitHub User',
         username: response.data.login,
-        gitHubToken: githubAcessToken
-      })
-      await newUser.save()
+        gitHubToken: githubAccessToken
+      });
+      await newUser.save();
     }
-
     const option = {
       httpOnly: true,
       expires: new Date(Date.now() + 60 * 60 * 1000)
     }
 
-    res.cookie('accessToken', userAccessToken, option).redirect(`${process.env.FRONTEND_URL}`)
+    res.cookie('accessToken', userAccessToken, option);
+    res.json({ success: true, token: userAccessToken });
   } catch (error) {
-    console.log(error)
+    //console.log(error);
     return res.status(500).json({
       success: false,
       message: 'Something went wrong !!',
       error: error
-    })
+    });
   }
-}
+};
 
-export { callback, githubAuth }
+export { callback, githubAuth };
