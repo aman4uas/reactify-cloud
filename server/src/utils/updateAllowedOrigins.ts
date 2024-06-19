a963c8e85f9e833b7fd8220d2f246503e1fb3f22
aman4uas <aman4u.as@gmail.com>
2024-06-19 11:45:37 +0530

    update

--- server/src/utils/updateAllowedOrigins.ts
+++ server/src/utils/updateAllowedOrigins.ts
@@ -3,7 +3,15 @@ import axios from 'axios'
 const updateAllowedOrigins = async (allowedOrigins: string[]) => {
   try {
     const response = await axios.get('https://api.github.com/meta')
-    const githubDomains = [...response.data.hooks, ...response.data.web]
+    const githubIPs = [...response.data.hooks, ...response.data.web]
+    const githubDomains = []
+    const length = githubIPs.length
+    for(let i=0; i<length; i++){
+      const httpDomain = `http://${githubIPs[i]}`
+      const httpsDomain = `https://${githubIPs[i]}`
+      githubDomains.push(httpDomain)
+      githubDomains.push(httpsDomain)
+    }
     allowedOrigins.push(...githubDomains)
   } catch (error) {
     console.log("Error updating GitHub IPs", error)
